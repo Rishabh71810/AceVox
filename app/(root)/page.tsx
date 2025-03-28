76dac62b0784a1346678a17b83a73e6adc69138e
Rishabh <rishabh19772004@gmail.com>
2025-03-28 18:08:07 +0530

    home page complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import Image from 'next/image'
+import { dummyInterviews } from '@/constants'
+import InterviewCard from '@/components/InterviewCard'
 const page = () => {
   return (
     <>
@@ -12,9 +16,29 @@ const page = () => {
             Practice interviews with AI, get feedback, and land your dream job with Acevox.
             </p>
             <Button asChild className='btn-primary max-sm:w-full'>
-            
+            <Link href="/interview">Start the Interview</Link>
             </Button>
          </div>
+<Image src="/robot.png" alt="robot" width={400} height={400} className='max-sm:hidden' />
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+       <h2>Your Interviews</h2>
+       <div className='interviews-section'>
+       {dummyInterviews.map((interview) => (
+         <InterviewCard {...interview} key = {interview.id}/>
+        ))}
+        {/* <p> You haven&apos;t taken any interviews yet</p> */}
+       </div>
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+       <h2>
+        Taken a Interview
+       </h2>
+       <div className="interviews-section">
+        {dummyInterviews.map((interview) => (
+         <InterviewCard {...interview} key = {interview.id}/>
+        ))}
+       </div>
       </section>
     </>
   )
